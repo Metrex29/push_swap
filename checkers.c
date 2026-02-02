9aacce60cffc01ae7d6cc88b97e8e4e73ddec268
Metrex29 <carlospicon80@gmail.com>
2026-02-02 19:30:49 +0100

    subida

--- checkers.c
+++ checkers.c
@@ -1,16 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init_stack.c                                       :+:      :+:    :+:   */
+/*   checkers.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: raulp <raulp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/02 18:22:08 by raulp             #+#    #+#             */
-/*   Updated: 2026/02/02 19:11:51 by raulp            ###   ########.fr       */
+/*   Created: 2026/02/02 19:11:44 by raulp             #+#    #+#             */
+/*   Updated: 2026/02/02 19:12:01 by raulp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-
-
+int check_duplicates(t_stack_node *stack, int n)
+{
+	if(!stack)
+		return 0;
+	while (stack)
+	{
+		if(stack ->value == n)
+			return 1;
+		stack = stack -> next;
+	}
+	return 0;
+}
