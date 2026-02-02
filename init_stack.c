8246fa8dba936963c5f72bd6181e07cd126407c8
raul <carlospicon80@gmail.com>
2026-02-02 18:59:38 +0100

    subida

--- init_stack.c
+++ init_stack.c
@@ -1,32 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   init_stack.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: raulp <raulp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/15 11:35:31 by cpicon-m          #+#    #+#             */
-/*   Updated: 2026/02/02 15:57:00 by raulp            ###   ########.fr       */
+/*   Created: 2026/02/02 18:22:08 by raulp             #+#    #+#             */
+/*   Updated: 2026/02/02 18:48:41 by raulp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "push_swap.h"
 
-# include <stdlib.h>
-# include <limits.h>
-# include <stdbool.h>
-# include <unistd.h>
 
-typedef struct s_stack_node
+int check_duplicates(t_stack_node *stack, int n)
 {
-	int					value; // El número a ordenar
-	int					index; // Su posición ideal (lo usaremos luego)
-	struct s_stack_node	*next;
-	struct s_stack_node	*prev;
-}	t_stack_node;
-
-// Prototipos
-t_stack_node	*ft_new_node(int value);
-
-#endif
+	if(!stack)
+		return 0;
+	while (stack)
+	{
+		if(stack ->value == n)
+			return 1;
+		stack = stack -> next;
+	}
+	return 0;
+}
--- nodes.c
+++ nodes.c
@@ -3,28 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   nodes.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cpicon-m <cpicon-m@student.42.fr>          +#+  +:+       +#+        */
+/*   By: raulp <raulp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 15:15:37 by cpicon-m          #+#    #+#             */
-/*   Updated: 2026/01/19 15:12:51 by cpicon-m         ###   ########.fr       */
+/*   Updated: 2026/02/02 18:23:54 by raulp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+/**
+ * ft_new_node - Create and initialize a new stack node
+ * @value: Integer value to store in the node
+ *
+ * Allocates and returns a pointer to a newly-created t_stack_node containing
+ * the given value. All pointer/linking fields are initialized to NULL and any
+ * auxiliary fields are set to sensible defaults (e.g., zeroed). Returns NULL
+ * if memory allocation fails.
+ *
+ * The caller takes ownership of the returned node and is responsible for
+ * freeing it. Time complexity: O(1). No global state is modified by this call.
+ */
+t_stack_node *ft_new_node(int value)
+{
+	t_stack_node *node;
+
+	node = malloc(sizeof(t_stack_node));
+	if (!node)
+		return (NULL);
+	node->value = value;
+	node->index = -1;
+	node->next = NULL;
+	node->prev = NULL;
+	return (node);
+}
+
 /*
 this function calculate the length of a stack
 */
-int	stack_len(t_stack_node *stack)
+int stack_len(t_stack_node *stack)
 {
-	int	count;
+	int count;
 
 	count = 0;
-	if(!stack)
+	if (!stack)
 		return (0);
 	while (stack)
 	{
-		stack = stack -> next;
+		stack = stack->next;
 		count++;
 	}
 	return (count);
@@ -33,33 +59,33 @@ int	stack_len(t_stack_node *stack)
 /*
 this function finds the last node value of a stack
 */
-t_stack_node	*find_last(t_stack_node *stack)
+t_stack_node *find_last(t_stack_node *stack)
 {
-	if(!stack)
+	if (!stack)
 		return (NULL);
-	while (stack -> next)
-		stack = stack -> next;
+	while (stack->next)
+		stack = stack->next;
 	return (stack);
 }
 
 /*
 this function finds the min node value of the stack
 */
-t_stack_node	*find__min(t_stack_node *stack)
+t_stack_node *find_min(t_stack_node *stack)
 {
 	// this is the  value min
-	long	min;
+	long min;
 	// this is the pointer to the min
 	t_stack_node *min_node;
-	
-	if(!stack)
+
+	if (!stack)
 		return (NULL);
 	// asign value to var
 	min = stack->value;
 	min_node = stack;
-	while(stack)
+	while (stack)
 	{
-		if(stack -> value < min)
+		if (stack->value < min)
 		{
 			min = stack->value;
 			min_node = stack;
@@ -69,7 +95,25 @@ t_stack_node	*find__min(t_stack_node *stack)
 	return (min_node);
 }
 
+void add_node_back(t_stack_node **stack, t_stack_node *new_node)
+{
+	t_stack_node *last_node;
 
+	if (!stack || !new_node)
+		return;
+		
+	if (!*stack)
+	{
+		*stack = new_node;
+		return;
+	}
+	// Buscamos el nodo que está actualmente al final
+	last_node = find_last(*stack);
 
+	// El que era el último ahora apunta al nuevo como su siguiente
+	last_node->next = new_node;
 
+	// El nuevo apunta al que era el último como su anterior
+	new_node->prev = last_node;
+}
 
